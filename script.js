b61a15474f16aab096d23ad120b24ce234e31107
Harshvardhan Singh Shekhawat <150536314+shekhawat4u@users.noreply.github.com>
2024-02-06 08:58:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scroll", window.scrollY > 0);
+});
+
+const faqs = document.querySelectorAll(".faq");
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("open");
+
+    const icon = faq.querySelector(".faq__icon i");
+    if (icon.className === "bx bx-plus") {
+      icon.className = "bx bx-minus";
+    } else {
+      icon.className = "bx bx-plus";
+    }
+  });
+});
+
+const menu = document.querySelector(".nav__menu");
+const menuBtn = document.querySelector("open-menu-btn");
+const closeBtn = document.querySelector("close-menu-btn");
+
+menuBtn.addEventListener("click", () => {
+  menu.style.display = "flex";
+  closeBtn.style.display = "inline-block";
+  menuBtn.style.display = "none";
+});
+
+const closeNav = () => {
+  menu.style.display = "none";
+  closeBtn.style.display = "none";
+  menuBtn.style.display = "inline-block";
+};
+
+closeBtn.addEventListener("click", closeNav);
